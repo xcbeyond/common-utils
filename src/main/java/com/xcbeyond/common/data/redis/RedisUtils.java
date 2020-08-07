cd84a8d26b019027a5fc76e4cc6b9ca72bcb267b
xuchao <xuchao@wondersoft.cn>
2020-08-07 15:29:15 +0800

    调整RedisUtils

--- src/main/java/com/xcbeyond/common/data/redis/RedisUtils.java
+++ src/main/java/com/xcbeyond/common/data/redis/RedisUtils.java
@@ -3,10 +3,12 @@ package com.xcbeyond.common.data.redis;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.support.atomic.RedisAtomicLong;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 
 /**
@@ -17,13 +19,11 @@ import java.util.concurrent.TimeUnit;
  */
 @Component
 public class RedisUtils {
-
     @Autowired
     private RedisTemplate<String, String> redisTemplate;
 
     /**
      * 读取缓存
-     *
      * @param key
      * @return
      */
@@ -32,7 +32,19 @@ public class RedisUtils {
     }
 
     /**
+     * 获取所有以prefix开头的key
+     * @param prefix 前缀key
+     * @return
+     */
+    public Set<String> getKeysByPrefix(String prefix){
+        return redisTemplate.keys(prefix + "*");
+    }
+
+    /**
      * 写入缓存
+     * @param key
+     * @param value
+     * @return
      */
     public boolean set(final String key, String value) {
         boolean result = false;
@@ -46,22 +58,47 @@ public class RedisUtils {
     }
 
     /**
-     * 写入缓存并设置缓存过期时间 (分钟)
-     * wzd
+     * 写入缓存并设置缓存过期时间
+     * @param key
+     * @param value
+     * @param expiration 过期时间，秒
+     * @return
      */
-    public boolean setCacheValueForTime(final String key, String value,long time) {
+    public boolean set(final String key, String value,long expiration) {
         boolean result = false;
         try {
-            redisTemplate.opsForValue().set(key,value,time,TimeUnit.MINUTES);
+            redisTemplate.opsForValue().set(key, value, expiration, TimeUnit.SECONDS);
             result = true;
         } catch (Exception e) {
             e.printStackTrace();
         }
         return result;
     }
+
+    /**
+     * 写入缓存,并带自动序列的key
+     * @param key
+     * @param value
+     * @return
+     */
+    public boolean setAutoKey(final String key, String value) {
+        boolean result = false;
+        try {
+            RedisAtomicLong entityIdCounter = new RedisAtomicLong(key, redisTemplate.getConnectionFactory());
+            Long increment = entityIdCounter.getAndIncrement();
+
+            redisTemplate.opsForValue().set(key + ":" + increment, value);
+            result = true;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
     /**
      * 是否包含key
-     * wzd
+     * @param key
+     * @return
      */
     public boolean hasKey(final String key) {
         boolean result = false;
@@ -75,6 +112,9 @@ public class RedisUtils {
 
     /**
      * 更新缓存
+     * @param key
+     * @param value
+     * @return
      */
     public boolean getAndSet(final String key, String value) {
         boolean result = false;
@@ -89,6 +129,8 @@ public class RedisUtils {
 
     /**
      * 删除缓存
+     * @param key
+     * @return
      */
     public boolean delete(final String key) {
         boolean result = false;
@@ -102,7 +144,22 @@ public class RedisUtils {
     }
 
     /**
+     * 按照key的前缀删除
+     * @param prefix 前缀key
+     * @return
+     */
+    public void deleteByPrefix(final String prefix) {
+        Set<String> keys = redisTemplate.keys(prefix + "*");
+        if (!keys.isEmpty()) {
+            redisTemplate.delete(keys);
+        }
+    }
+
+    /**
      * 写入 list 以元素的形式
+     * @param key
+     * @param value
+     * @return
      */
     public boolean addList(final String key, String value) {
         boolean result = false;
@@ -164,6 +221,7 @@ public class RedisUtils {
     /**
      * 增加hash结构的数据
      * @param key
+     * @param map
      * @return
      */
     public boolean addHash(final String key, Map<String, Object> map){
@@ -180,6 +238,7 @@ public class RedisUtils {
     /**
      * 删除Hash结构的数据
      * @param key
+     * @param mapKey
      * @return
      */
     public boolean delHash(final String key, String mapKey){
@@ -193,10 +252,8 @@ public class RedisUtils {
         return result;
     }
 
-
     /**
      * 获取hash中的值
-     *
      * @param key
      */
     public Map<Object, Object> getHashSet(final String key) {
